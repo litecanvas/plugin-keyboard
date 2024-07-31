fcaf5eda33149fc68fbfe90eb7a719e7d6278d56
Luiz Bills <luizbills@pm.me>
2024-07-31 17:33:08 -0300

    improve plugin

--- demo/demo.js
+++ demo/demo.js
@@ -1,19 +1,3 @@
-# Keyboard plugin for litecanvas
-
-Plugin to handle keyboard events in [litecanvas](https://github.com/litecanvas/engine) games.
-
-## Install
-
-**NPM**: `npm i @litecanvas/plugin-keyboard`
-
-**CDN**: `https://unpkg.com/@litecanvas/plugin-keyboard/dist/dist.js`
-
-## Usage
-
-```js
-import litecanvas from "litecanvas"
-import pluginKeyboard from "@litecanvas/plugin-keyboard"
-
 let color, holding
 
 litecanvas({
@@ -49,15 +33,11 @@ function keyup(key) {
 
 function keypress(key) {
   switch (key) {
-    // press enter to change the background color
     case "enter":
       color++
       break
-
-    // press ESC to display a message
     case "escape":
       alert("Escape pressed")
       break
   }
 }
-```
--- src/index.js
+++ src/index.js
@@ -1,67 +1,79 @@
-/*! Keyboard plugin for litecanvas v0.0.1 by Luiz Bills | MIT Licensed */
+/*! Keyboard plugin for litecanvas v0.1.0 by Luiz Bills | MIT Licensed */
 window.pluginKeyboard = plugin
 
 /**
  * @param {LitecanvasInstance} engine
- * @param {LitecanvasPluginHelpers}
+ * @param {LitecanvasPluginHelpers} _
  * @param {any} config
  * @returns any
  */
-export default function plugin(engine, { colors, sounds, settings }, config) {
+export default function plugin(engine, _, config) {
   const keyDownList = new Map()
-  const keyPressList = new Map()
 
-  addEventListener("keydown", _listenKeydownEvent)
-  addEventListener("keyup", _listenKeyupEvent)
+  const defaults = {
+      listeners: {
+        /** @type {null|(key: string, ev:KeyboardEvent) => void} */
+        keydown: window.keydown,
+        /** @type {null|(key: string, ev:KeyboardEvent) => void} */
+        keyup: window.keyup,
+        /** @type {null|(key: string, ev:KeyboardEvent) => void} */
+        keypress: window.keypress,
+      },
+    },
+    events = ["keydown", "keyup", "keypress"],
+    time = () => performance.now()
 
+  config = Object.assign(defaults, config)
+
+  /**
+   * @param {KeyboardEvent} ev
+   */
+  addEventListener("keydown", (ev) => {
+    const key = _formatKey(ev.key)
+
+    engine.emit("keydown", key, ev)
+
+    keyDownList.set(key, time())
+  })
+
+  /**
+   * @param {KeyboardEvent} ev
+   */
+  addEventListener("keyup", (ev) => {
+    const key = _formatKey(ev.key)
+
+    if (_checkPressed(key)) {
+      engine.emit("keypress", key, ev)
+    }
+    engine.emit("keyup", key, ev)
+
+    keyDownList.delete(key)
+  })
+
+  for (const eventName of events) {
+    if ("function" === typeof config.listeners[eventName]) {
+      engine.listen(eventName, config.listeners[eventName], true)
+    }
+    console.log(window)
+  }
+
+  /**
+   * @param {string} key
+   * @returns {string}
+   */
   function _formatKey(key) {
-    key = key.toLowerCase().replace("arrow", "")
-    key = key === " " ? "space" : key // fix space bar
+    key = key.toLowerCase()
+
+    if (key.startsWith("arrow")) {
+      key = key.replace("arrow", "")
+    } else if (" " === key) {
+      key = "space"
+    }
+
     return key
   }
 
-  function _listenKeydownEvent(evt) {
-    let key = _formatKey(evt.key)
-    if (!keyDownList.has(key)) {
-      keyDownList.set(key, performance.now())
-    }
-  }
-
-  function _listenKeyupEvent(evt) {
-    let key = _formatKey(evt.key)
-    let time = performance.now() - keyDownList.get(key)
-
-    if (time <= 200) {
-      keyPressList.set(key, true)
-    }
-
-    keyDownList.delete(key)
-  }
-
-  engine.listen("update", () => {
-    keyPressList.clear()
-  })
-
-  addEventListener("blur", () => {
-    keyDownList.clear()
-    keyPressList.clear()
-  })
-
-  function iskeydown(key) {
-    return null == key ? keyDownList.size > 0 : keyDownList.has(key)
-  }
-
-  function iskeypressed(key) {
-    return null == key ? keyPressList.size > 0 : keyPressList.has(key)
-  }
-
-  const KB = {
-    iskeydown,
-    iskeypressed,
-  }
-
-  return {
-    KB,
-    KEYBOARD: KB,
+  function _checkPressed(key) {
+    return keyDownList.has(key) && time() - keyDownList.get(key) <= 200
   }
 }
